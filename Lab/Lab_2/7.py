da374896e3df3fc5d67620a041b5e26e26175b33
Furqan Ahmad <88136810+Furqan3@users.noreply.github.com>
2023-02-23 19:24:50 +0500

    Update 7.py

--- Lab/Lab_2/7.py
+++ Lab/Lab_2/7.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2 as cv
+import time as sec
 
 def resize(image):
     x=int(image.shape[0]/4)
@@ -14,15 +15,10 @@ def increase(image):
     x=int(image.shape[0]*4)
     y=int(image.shape[1]*4)
     one=np.ones((x,y),np.uint8)
-    for i in range(0,image.shape[0]):
-        for j in range(0,image.shape[1]-1):
-            one[i*4][j*4]=image[i][j]
-            a=image[i][j+1]
-            b=image[i][j]
-            x=int((a+b)/2)
-            one[(i*4)+3][(j*4)+3]=a
-            one[(i*4)+2][(j*4)+2]=x
-            one[(i*4)+1][(j*4)+1]=b
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            one[4*i:4*(i+1), 4*j:4*(j+1)] = image[i, j]
+           
     return one
 
 image=cv.imread('sample.png',0)
@@ -32,3 +28,4 @@ cv.imshow('Small Size',image2)
 image3=increase(image2)
 cv.imshow('Large Size',image3)
 cv.waitKey()
+
